82924cda25546581be35ce0e7aba63479e0fefc2
Pavel Khorev <khorev05@list.ru>
2026-03-03 23:56:05 +0700

    feat: add courses entities and migration

---
+++ PotteryClass/Data/Entities/CourseStudent.cs
@@ -0,0 +1,13 @@
+﻿namespace PotteryClass.Data.Entities
+{
+    public class CourseStudent
+    {
+        public Guid CourseId { get; set; }
+        public Course Course { get; set; } = null!;
+
+        public Guid UserId { get; set; }
+
+        public bool IsBlocked { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+    }
+}
---
+++ PotteryClass/Data/Entities/CourseTeacher.cs
@@ -0,0 +1,12 @@
+﻿namespace PotteryClass.Data.Entities
+{
+    public class CourseTeacher
+    {
+        public Guid CourseId { get; set; }
+        public Course Course { get; set; } = null!;
+
+        public Guid UserId { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; }
+    }
+}
